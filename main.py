4f03d33a77c96be00301cf9529997341ccdc10c9
Ian <sammythebeast18@gmail.com>
2020-10-17 21:29:02 -0400

    Basic firestore functionality

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask, request
+from google.cloud import firestore
 
 
 app = Flask(__name__)
+db = firestore.Client.from_service_account_json('crowdbudget-a5991e488c95.json')
+
+def get_budgets(location_id):
+    location_ref = db.collection(u"locations")
+    state = location_ref.document(location_id)
+    budgets = state.collection(u"budgets")
+    for doc in budgets.stream():
+        print(f"{doc.id}: {doc.to_dict()}")
+
 
 @app.route("/", methods=["GET", "POST"])
 def main_page():
@@ -11,4 +21,6 @@ def main_page():
     return "Hello World!\nNot a post :("
 
 if __name__ == "__main__":
-    app.run()
+    get_budgets("RI")
+    #app.run()
+
